9478779a3a5131b557a47635dd705b8fc39b2e35
Nikiput <nkita.putri@gmail.com>
2026-02-14 18:56:36 +0700

    add : navbar for homepage, suggestion page, and supportpage

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  standalone : true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class NavbarComponent {
+  isMenuOpen = false;
+  isScrolled = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Jika scroll lebih dari 20px, ubah status jadi true
+    this.isScrolled = window.scrollY > 20;
+  }
 }
