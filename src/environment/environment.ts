2daeeaa4c771c4bc342d590edf8d4504b3bdb669
crisvito <crisvitoc@gmail.com>
2026-02-18 11:09:31 +0700

    fix: title banner statistic

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,9 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api/v1.0/'
-};
-
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://viktorifit-backend-production.up.railway.app/api/v1.0/'
+//   apiUrl: 'http://localhost:8080/api/v1.0/'
 // };
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://viktorifit-backend-production.up.railway.app/api/v1.0/'
+};
